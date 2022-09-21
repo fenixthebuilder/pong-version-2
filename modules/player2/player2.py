c32aa0bdf0ec809d720af027eda60314c6b0b262
fenixthebuilder <104685500+fenixthebuilder@users.noreply.github.com>
2022-09-21 22:08:01 +0200

    Add files via upload

---
+++ modules/player2/player2.py
@@ -0,0 +1,25 @@
+STRETCH_WIDTH = 5
+STRETCH_LENGTH = 1
+PADDLE_COLOR= "white"
+
+class player2(object):
+    def __init__(self, turtle, speed, shape, shapesize, color, penup, goto, ycor, sety):
+        self.turtle = turtle.Turtle()
+        self.speed = speed(0)
+        self.shape = shape("square")
+        self.shapesize= shapesize(STRETCH_WIDTH, STRETCH_LENGTH)
+        self.color = color(PADDLE_COLOR)
+        self.penup = penup()
+        self.goto = goto(-350, 0)
+        self.ycor = ycor()
+        self.sety = sety()
+
+def up():
+    y = player2.ycor()
+    y += 20
+    player2.sety(y)
+
+def dw():
+    y = player2.ycor()
+    y -= 20
+    player2.sety(y)
